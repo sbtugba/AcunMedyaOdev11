83b47ea8c8ea10c4f54f51d2de70d7a976841dc5
sbtugba <sahbaztugba24@gmail.com>
2025-03-29 01:48:35 +0300

    Commit

---
+++ AcunMedyaOdev/AcunMedyaOdev/Car.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcunMedyaOdev
+{
+    internal class Car
+    {
+        public string Marka { get; set; }
+        public string Model { get; set; }
+        public double FuelConsumption { get; set; } 
+        public double TotalDistance { get; set; }   
+
+      
+        public double CalculateFuelConsumption()
+        {
+            return (TotalDistance / 100) * FuelConsumption;
+        }
+    }
+}
---
+++ AcunMedyaOdev/AcunMedyaOdev/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace AcunMedyaOdev
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Car> cars = new List<Car>
+        {
+            new Car { Marka = "Toyota", Model = "Corolla", FuelConsumption = 6.5, TotalDistance = 5000 },
+            new Car { Marka = "BMW", Model = "X5", FuelConsumption = 9.0, TotalDistance = 5000 },
+            new Car { Marka = "Mercedes", Model = "C200", FuelConsumption = 8.2, TotalDistance = 5000 }
+        };
+
+           
+            foreach (var car in cars)
+            {
+                double fuelUsed = car.CalculateFuelConsumption();
+                Console.WriteLine($"Marka: {car.Marka}, Model: {car.Model}, 100 km'de Yaktığı Yakıt: {car.FuelConsumption} litre, Toplam Yakıt Tüketimi: {fuelUsed} litre");
+            }
+        }
+    }
+}
